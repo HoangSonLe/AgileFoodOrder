5db899208bb3cab98902a818b1d02a37f4bbdfc0
Thanhhv317 <thanhhoang317@gmail.com>
2019-10-14 12:27:41 +0700

    231-30-Design-layout-list-of-order(admin-side) complete design layout for admin side

---
+++ OrderFoodLast/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderFoodLast.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Area("Admin")]
+        [Route("Admin/Order/Edit/{id?}")]
+        public IActionResult Edit(int id)
+        {
+            return View("Detail");
+        }
+
+    }
+}
