be2c7795e90badd75d3c994a9bfc95bd6da541eb
minhlc98 <=>
2019-10-19 11:16:46 +0700

    update password of employee and customer

---
+++ OrderFoodLast/Database/updatePass.sql
@@ -0,0 +1,8 @@
+update Employee set Password='b3003135720325e541923035f440ea96' where EmployeeID=1
+update Employee set Password='a672a1f9abc161d1acbdeeab9b91eff7' where EmployeeID=2
+
+update Customer set Password='fd861f5ed4f38e52128325f07b2fbd49' where CustomerID=1
+update Customer set Password='36fc0417a7a5beab37e3e490c4543d55' where CustomerID=2
+
+
+
