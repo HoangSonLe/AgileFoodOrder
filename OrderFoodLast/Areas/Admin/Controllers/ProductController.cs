79913be553fd5b2bab1a8939ad2a1f4fc83b7738
HoangSonLe <hoangsonle211@gmail.com>
2019-10-30 19:27:40 +0700

    FO-16 Product List page. Done FO-17 Show list product. Show data. Still not paginaiton

--- OrderFoodLast/Areas/Admin/Controllers/ProductController.cs
+++ OrderFoodLast/Areas/Admin/Controllers/ProductController.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using OrderFoodLast.Models;
+using OrderFoodLast.Areas.Admin.ModelViews;
+using Microsoft.EntityFrameworkCore;
 
 namespace OrderFoodLast.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin")] 
     public class ProductController : Controller
     {
+        private readonly OrderFoodContext _ctx;
+        private readonly IMapper _mapper;
+        public ProductController(OrderFoodContext orderFoodContext,IMapper mapper)
+        {
+            _ctx = orderFoodContext;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = _ctx.Product.Include(p => p.Category).ToList();
+            List<ProductViewAdmin> productViews = new List<ProductViewAdmin>();
+            foreach(var p in products)
+            {
+                try
+                {
+                    ProductViewAdmin productView = new ProductViewAdmin();
+                    productView.ProductId = p.ProductId;
+                    productView.ProductName = p.ProductName;
+                    productView.ProductImage = p.ProductImage;
+                    productView.Price = int.Parse(p.Price.ToString());
+                    productView.Quantity = int.Parse(p.Quantity.ToString());
+                    productView.Status = (p.Status == 0) ? "No Active" : "Active";
+                    productView.Category = p.Category.Name;
+                    productViews.Add(productView);
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+            return View(productViews);
         }
     }
 }
---
+++ OrderFoodLast/Areas/Admin/ModelViews/ProductViewAdmin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderFoodLast.Areas.Admin.ModelViews
+{
+    public class ProductViewAdmin
+    {
+        [Key]
+        [Display(Name = "Mã hàng hóa")]
+        public int ProductId { get; set; }
+        [Display(Name = "Tên hàng hóa")]
+        public string ProductName { get; set; }
+        [MaxLength(150)]
+        [Display(Name = "Hình")]
+        public string ProductImage { get; set; }
+        //[Display(Name = "Đơn giá")]
+        public int Price { get; set; }
+        [Display(Name = "Số lượng")]
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+        public string Category { get; set; }
+    }
+}
