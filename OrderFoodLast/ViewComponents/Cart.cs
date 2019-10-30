68357a59e86f9f71b42b99c5591fdfb93ac77b76
Quoc Ky <dqky.hcmup@gmail.com>
2019-10-30 15:53:34 +0700

    update file Cart viewComponet

---
+++ OrderFoodLast/ViewComponents/Cart.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderFoodLast.Helper;
+using OrderFoodLast.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderFoodLast.ViewComponents
+{
+    public class Cart : ViewComponent
+    {
+        public List<CartItem> Carts
+        {
+            get
+            {
+                var data = HttpContext.Session.GetObject<List<CartItem>>("Cart");
+                if (data == null)
+                {
+                    data = new List<CartItem>();
+                }
+                return data;
+            }
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Default", Carts);
+        }
+    }
+}
