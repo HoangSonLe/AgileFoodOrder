60eb4cfe00a52ca58a00e749d789d51536805edb
Quoc Ky <dqky.hcmup@gmail.com>
2019-10-30 15:41:59 +0700

    complete design layout detail product

--- OrderFoodLast/Areas/Admin/Controllers/ProductController.cs
+++ OrderFoodLast/Areas/Admin/Controllers/ProductController.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using OrderFoodLast.Models;
 
 namespace OrderFoodLast.Areas.Admin.Controllers
 {
+    
     public class ProductController : Controller
     {
+        private readonly OrderFoodContext _ctx;
+        private readonly IMapper _mapper;
+
+        public ProductController(OrderFoodContext db, IMapper mapper)
+        {
+            _ctx = db; _mapper = mapper;
+        }
         [Area("Admin")]
         public IActionResult Index()
         {
             return View();
         }
-
-
+        [Area("Admin")]
+        [Route("Admin/Product/{id}")]
+        public IActionResult Detail(int id)
+        {
+            var model = _ctx.Product.Where(p => p.ProductId == id) as Product;
+            return View(model);
+        }
 
     }
 }
