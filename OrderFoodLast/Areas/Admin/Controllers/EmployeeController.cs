bacbff08239bfb91b855ef3a6f23eecfacc1372c
Thanhhv317 <thanhhoang317@gmail.com>
2019-10-31 16:39:20 +0700

    FO-4: Getting data from database and display it to the view

--- OrderFoodLast/Areas/Admin/Controllers/EmployeeController.cs
+++ OrderFoodLast/Areas/Admin/Controllers/EmployeeController.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using OrderFoodLast.Areas.Admin.Models;
+using OrderFoodLast.Models;
 
 namespace OrderFoodLast.Areas.Admin.Controllers
 {
     public class EmployeeController : Controller
     {
+
+        private readonly OrderFoodContext _ctx;
+        private readonly IMapper _mapper;
+        public EmployeeController(OrderFoodContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
         [Area("Admin")]
         public IActionResult Index()
         {
-            return View();
+            // danh sach employee trong model 
+            var employeeList = _ctx.Employee.ToList();
+            
+            var employees = _mapper.Map<List<EmployeeView>>(employeeList);
+            return View(employees);
         }
     }
 }
---
+++ OrderFoodLast/Areas/Admin/Models/EmployeeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderFoodLast.Areas.Admin.Models
+{
+    public class EmployeeView
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+       
+    }
+}
