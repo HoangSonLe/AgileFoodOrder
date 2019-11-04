14e7e2cc59e49634b790f3a046af5da480ec2561
HoangPhuongNguyen <nguyenphuonghoang.1998@gmail.com>
2019-11-04 18:04:20 +0700

    update function create employee

--- OrderFoodLast/Areas/Admin/Controllers/EmployeeController.cs
+++ OrderFoodLast/Areas/Admin/Controllers/EmployeeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OrderFoodLast.Helper;
 using OrderFoodLast.Models;
 
 namespace OrderFoodLast.Areas.Admin.Controllers
@@ -13,6 +15,13 @@ namespace OrderFoodLast.Areas.Admin.Controllers
     [Area("Admin")]
     public class EmployeeController : Controller
     {
+        private readonly OrderFoodContext _ctx;
+
+        public EmployeeController(OrderFoodContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -34,21 +43,31 @@ namespace OrderFoodLast.Areas.Admin.Controllers
         [Route("Admin/Employee/Create")]
         public IActionResult Create(IFormCollection data)
         {
-            Employee emp = new Employee()
+            try
             {
-                LastName = data["lastName"],
-                FirstName = data["firstName"],
-                UserName = data["userName"],
-                Password = MD5Hash(data["password"]),
-                Address = data["address"],
-                Phone = data["phone"],
-                BirthDate = Convert.ToDateTime(data["birthday"]),
-                Role = int.Parse(data["role"]),
-                ManagerId = int.Parse(data["managerId"]),
-                Status = int.Parse(data["status"])
-            };
-            return Ok(emp);
-            //return View("Index");
+                var info = HttpContext.Session.GetObject<LoginInfo>("Info");
+                Employee emp = new Employee();
+                emp.LastName = data["lastName"];
+                emp.FirstName = data["firstName"];
+                emp.UserName = data["userName"];
+                emp.Password = MD5Hash(data["password"]);
+                emp.Address = data["address"];
+                emp.Email = data["email"];
+                emp.Phone = data["phone"];
+                emp.BirthDate = DateTime.Parse(data["birthday"]);
+                emp.Role = int.Parse(data["role"]);
+                emp.ManagerId = int.Parse(data["managerId"]);
+                emp.Status = int.Parse(data["status"]);
+                emp.CreatedDate = DateTime.Now;
+                emp.CreatedBy = info.UserID;
+                _ctx.Employee.Add(emp);
+                _ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
         public static string MD5Hash(string text)
