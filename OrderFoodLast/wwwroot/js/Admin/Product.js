abdb1c4df39b1bf6d445a8539af04454ae9535d1
minhlc98 <=>
2019-11-07 09:23:25 +0700

    Code btn Prveious and Next

--- OrderFoodLast/wwwroot/js/Admin/Product.js
+++ OrderFoodLast/wwwroot/js/Admin/Product.js
@@ -1,8 +1,11 @@
 ﻿$(function () {
+    var pageNumber = parseInt($("#inputPagination").val()) - 1;
+    if (pageNumber < 0) $(".Pre").addClass("disabled");
 //var selectedVal = $("#inputState option:selected").val();
 //alert(selectedVal);
     $("#inputPagination").on("change", function () {
-        var pageNumber = this.value;
+       
+        var pageNumber = this.value;        
         var metaTitle = $("#inputState option:selected").val();
         $.ajax({
             url: "/Admin/Product/SelectProduct",
@@ -14,8 +17,52 @@
             success: function (data) {
                 $(".bodyContent").html("");
                 $(".bodyContent").html(data);
+                $(".Pre").addClass("disabled");
             }
         });
+    });
+    $(".btnNext").on("click", function (e) {
+        e.preventDefault();
+        var pageNumber = parseInt($("#inputPagination").val()) + 1;
+        
+        var metaTitle = $("#inputState option:selected").val();
+        $.ajax({
+            url: "/Admin/Product/SelectProduct",
+            type: "GET",
+            data: {
+                page: pageNumber,
+                metaTitle: metaTitle
+            },
+            success: function (data) {
+                $(".bodyContent").html("");
+                $(".bodyContent").html(data);
+
+            }
+        });
+    });
+    $(".btnPrevious").on("click", function (e) {
+        e.preventDefault();
+        var pageNumber = parseInt($("#inputPagination").val()) - 1;
+        $(".Pre").attr("disabled", true);
+        var metaTitle = $("#inputState option:selected").val();
+        if (pageNumber >= 0) {
+            $.ajax({
+                url: "/Admin/Product/SelectProduct",
+                type: "GET",
+                data: {
+                    page: pageNumber,
+                    metaTitle: metaTitle
+                },
+                success: function (data) {
+                    $(".bodyContent").html("");
+                    $(".bodyContent").html(data);
+                }
+            });
+        }
+        else {
+            Swal.fire('Đã tới trang 1 !');
+        }
+        
     });
     $(".sua-loai").click(function () {
         var maLoai = $(this).data("maloai");
